5bd70890571f55a66a65955a40e0c0355e872bde
lukasjaronis <jaronisluke13@gmail.com>
2019-12-17 14:57:15 -0500

    login styling + app styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 
 const AppStyles = styled.div`
 
+display: flex;
+justify-content: center;
+align-items: center;
 height: 100vh;
 margin: 0 auto;
 padding: 5rem;
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
+import styled from 'styled-components';
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+
+const LoginStyles = styled.div`
+
+height: 30rem;
+width: 30rem;
+display: flex;
+justify-content: center;
+align-items: center;
+border: 1px solid #d8c3a5;
+-webkit-box-shadow: 5px 5px 15px 5px #fff; 
+box-shadow: 5px 5px 15px 5px #fff;
+border-radius: 3rem;
+background: #E98074;
+
+h1 {
+    color: #fff;
+    letter-spacing: 2px;
+}
+
+.inputContainer {
+    margin: 2rem;
+}
+
+button {
+    width: 7rem;
+    height: 2.3rem;
+    font-size: 1rem;
+    font-weight: 900;
+    letter-spacing: 2px;
+}
+
+`;
 
 const useStyles = makeStyles(theme => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
-      width: 200
+      width: 200,
     }
   }
 }));
@@ -40,12 +73,16 @@ class Login extends Component {
   render() {
     
     return (
+        <LoginStyles>
       <div>
+      <div>
+      <h1>Sign In</h1>
+      </div>
         <form onSubmit={this.loggingIn} className={useStyles.root} noValidate autoComplete="off">
-        <div>
+        <div className="inputContainer">
         <TextField name="username" onChange={this.handleChange} value={this.state.credentials.username} id="outlined-basic" label="Username" variant="outlined" />
         </div>
-        <div>
+        <div className="inputContainer">
         <TextField name="password" onChange={this.handleChange} value={this.state.credentials.password} id="outlined-basic" label="Password" variant="outlined" />
         </div>
         <div>
@@ -54,6 +91,7 @@ class Login extends Component {
         </div>
         </form>
       </div>
+      </LoginStyles>
     );
   }
 }
